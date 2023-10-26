984effb7c2c9c27280a2cef7b621b29228e4d46f
TomoyaWaniko <alligatorfree12@gmail.com>
2023-10-25 21:01:47 -0700

    Commit

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -5,12 +5,13 @@ import ReactDOM from "react-dom/client";
 // ./index.css is where we add all our css for our app.
 import "./index.css";
 // <App /> is in the render function below.
-import App from "./App";
+// import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <h1>Hello World</h1>
     <App />
   </React.StrictMode>
 );
