ef610101a838f6729fd90b9fd7c4eaa1a42a3fed
TomoyaWaniko <alligatorfree12@gmail.com>
2023-11-01 14:31:00 -0700

    Contribution

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -12,6 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <h1>Hello World</h1>
+    <p>This is a test....!!!</p>
     <App />
   </React.StrictMode>
 );
