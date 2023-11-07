4b3d0bffd0e309fe38ee4d076dc42bbe1bf32b6f
TomoyaWaniko <alligatorfree12@gmail.com>
2023-11-07 15:59:23 -0800

    Commit

--- react-tutorial-youtube/src/App.js
+++ react-tutorial-youtube/src/App.js
@@ -1,7 +1,18 @@
-import "./App.css";
+import { useState } from "react";
+import TodoList from "./TodoList";
 
 function App() {
-  return <div className="App"></div>;
+  const [todos, setTodos] = useState(["Todo1", "Todo2"]);
+
+  return (
+    <>
+      <TodoList todos={todos} />
+      <input type="text"></input>
+      <button>Add your task</button>
+      <button>delete finished task</button>
+      <div>number of unfinished tasks:0</div>
+    </>
+  );
 }
 
 export default App;
---
+++ react-tutorial-youtube/src/TodoList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const TodoList = ({ todos }) => {
+  return todos.map((todo) => 
+    <Todo todo={todo} />;
+  );
+};
+
+export default TodoList;
