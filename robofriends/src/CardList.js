8bb0341a6e27f4ea9c9fee8d076273cd84619175
TomoyaWaniko <alligatorfree12@gmail.com>
2023-11-13 14:41:11 -0800

    test

---
+++ robofriends/src/CardList.js
@@ -0,0 +1,7 @@
+import React from "react";
+import Card from "./Card";
+
+const CardList = ({ robots }) => {
+  const CardComponent = robots.map(user);
+  return <div></div>;
+};
--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -2,17 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Card from "./Card";
+import CardList from "./CardList";
 import reportWebVitals from "./reportWebVitals";
 import "tachyons";
+import { robots } from "./robots";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <div>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      <CardList robots={robots} />
     </div>
   </React.StrictMode>
 );
