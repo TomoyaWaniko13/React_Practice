d082ef0bfdf5863135abab5ac957192ffa4b6092
TomoyaWaniko <alligatorfree12@gmail.com>
2023-11-12 18:20:52 -0800

    Test

---
+++ progate_react_practice/src/Language.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class Language extends React.Component {
+  render() {
+    <div className="language-item">
+      <div className="language-name">HTML & CSS</div>
+      <img className="language-image" src></img>
+    </div>;
+  }
+}
