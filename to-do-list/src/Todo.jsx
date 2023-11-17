b63cfdb3c5a3947f8c86172977ba194ecb8edac3
TomoyaWaniko <alligatorfree12@gmail.com>
2023-11-17 15:41:29 -0800

    Create add functionality

--- to-do-list/src/Todo.jsx
+++ to-do-list/src/Todo.jsx
@@ -2,14 +2,31 @@ import { useState } from 'react';
 import './Todo.css';
 
 export const Todo = () => {
+  const [todoText, setTodoText] = useState();
+
   const [unfinishedTasks, setUnfinishedTasks] = useState(['Todo1', 'Todo2']);
-  const [complteTasks, setCompleteTasks] = useState(['Todo1', 'Todo2']);
+  const [finishedTasks, setFinishedTasks] = useState(['Todo1', 'Todo2']);
+
+  const onChangeTodoText = event => setTodoText(event.target.value);
+
+  const onClickAdd = () => {
+    if (!todoText) return;
+    const newTasks = [...unfinishedTasks, todoText];
+    setUnfinishedTasks(newTasks);
+    setTodoText('');
+  };
 
   return (
     <>
       <div id="add-area">
-        <input id="input-area" type="text" placeholder="Enter your task" />
-        <button id="add-button">Add</button>
+        <input
+          id="input-area"
+          type="text"
+          placeholder="Enter your task"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
+        <button onClick={onClickAdd}>Add</button>
       </div>
       <div id="unfinished-area">
         <p className="title">Unfinisehd Tasks</p>
@@ -28,7 +45,7 @@ export const Todo = () => {
       <div id="finished-area">
         <p className="title">Finished Tasks</p>
         <ul>
-          {complteTasks.map(todo => {
+          {finishedTasks.map(todo => {
             <li key={todo}>
               <div className="list-row">
                 <p className="todo-item">{todo}</p>
