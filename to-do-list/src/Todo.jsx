3a668df2a85aeeba3b43868d8e6e17b0a954a073
TomoyaWaniko <alligatorfree12@gmail.com>
2023-11-17 13:15:31 -0800

    Create HTML and CSS

--- to-do-list/src/Todo.jsx
+++ to-do-list/src/Todo.jsx
@@ -1,6 +1,36 @@
+import "./Todo.css";
 
 export const Todo = () => {
   return (
-   <div>hoge</div>
+   <>
+   <div id="add-area">
+    <input id="input-area" type="text" placeholder="Enter your task" />
+    <button id="add-button">Add</button>
+   </div>
+   <div id="unfinished-area">
+    <p>Unfinisehd Tasks</p>
+    <ul>
+      <li class="unfinished-li">
+        <p>TOOD</p>
+        <button>done</button>
+        <button>delete</button>
+      </li>
+      <li className="unfinished-li">
+        <p>TOOD</p>
+        <button>done</button>
+        <button>delete</button>
+      </li>
+    </ul>
+   </div>
+   <div id="finished-area">
+    <p>Finished Tasks</p>
+    <ul>
+      <li>
+        <p>TOOD</p>
+        <button>undo</button>
+      </li>
+      </ul>
+   </div>
+   </>
   );
 };
