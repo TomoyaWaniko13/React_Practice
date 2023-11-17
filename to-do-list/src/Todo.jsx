9ac82a96952826ebd213f25d60b068d4aee47385
TomoyaWaniko <alligatorfree12@gmail.com>
2023-11-17 14:51:34 -0800

    Commit

--- to-do-list/src/Todo.jsx
+++ to-do-list/src/Todo.jsx
@@ -1,36 +1,43 @@
-import "./Todo.css";
+import { useState } from 'react';
+import './Todo.css';
 
 export const Todo = () => {
+  const [unfinishedTasks, setUnfinishedTasks] = useState(['Todo1', 'Todo2']);
+  const [complteTasks, setCompleteTasks] = useState(['Todo1', 'Todo2']);
+
   return (
-   <>
-   <div id="add-area">
-    <input id="input-area" type="text" placeholder="Enter your task" />
-    <button id="add-button">Add</button>
-   </div>
-   <div id="unfinished-area">
-    <p>Unfinisehd Tasks</p>
-    <ul>
-      <li class="unfinished-li">
-        <p>TOOD</p>
-        <button>done</button>
-        <button>delete</button>
-      </li>
-      <li className="unfinished-li">
-        <p>TOOD</p>
-        <button>done</button>
-        <button>delete</button>
-      </li>
-    </ul>
-   </div>
-   <div id="finished-area">
-    <p>Finished Tasks</p>
-    <ul>
-      <li>
-        <p>TOOD</p>
-        <button>undo</button>
-      </li>
-      </ul>
-   </div>
-   </>
+    <>
+      <div id="add-area">
+        <input id="input-area" type="text" placeholder="Enter your task" />
+        <button id="add-button">Add</button>
+      </div>
+      <div id="unfinished-area">
+        <p className="title">Unfinisehd Tasks</p>
+        <ul>
+          {unfinishedTasks.map(todo => (
+            <li key={todo}>
+              <div className="list-row">
+                <p className="todo-item">{todo}</p>
+                <button>done</button>
+                <button>delete</button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div id="finished-area">
+        <p className="title">Finished Tasks</p>
+        <ul>
+          {complteTasks.map(todo => {
+            <li key={todo}>
+              <div className="list-row">
+                <p className="todo-item">{todo}</p>
+                <button>undo</button>
+              </div>
+            </li>;
+          })}
+        </ul>
+      </div>
+    </>
   );
 };
