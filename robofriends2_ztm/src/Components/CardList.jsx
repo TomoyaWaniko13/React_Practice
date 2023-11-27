6cf71c4046e4425ff3d11d22842014f2416bf3f0
TomoyaWaniko <alligatorfree12@gmail.com>
2023-11-27 15:17:46 -0800

    Create Card.jsx and CardList.jsx

---
+++ robofriends2_ztm/src/Components/CardList.jsx
@@ -0,0 +1,16 @@
+import {Card} from "./Card";
+
+export const CardList = () =>{
+    return(
+        <div className="App flex flex-wrap justify-center">
+            <Card name={"Neko"} email={"kevin@gmail.com"} />
+            <Card name={"Neko"} email={"james@gmail.com"}/>
+            <Card name={"Neko"} email={"paul@gmail.com"}/>
+            <Card name={"Neko"} email={"vwefae@gmail.com"}/>
+            <Card name={"Neko"} email={"kevnfafa@gmail.com"}/>
+            <Card name={"Neko"} email={"faijgareo@gmail.com"}/>
+            <Card name={"Neko"} email={"fajojjteait@gmail.com"}/>
+            <Card name={"Neko"} email={"ffafdafeafaotjeoreorjeoj@gmail.com"}/>
+        </div>
+    )
+}
