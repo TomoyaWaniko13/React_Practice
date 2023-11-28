e28d9ec89868d05a57527235519e4871426b9617
TomoyaWaniko <alligatorfree12@gmail.com>
2023-11-28 15:25:08 -0800

    make map

--- robofriends2_ztm/src/Components/Card.jsx
+++ robofriends2_ztm/src/Components/Card.jsx
@@ -3,7 +3,8 @@ export const Card = ({id, name, email}) => {
 
         <div
             className={"bg-blue-300 m-2 p-3 rounded-3xl" +
-                " w-full sm:w-full md:w-full lg:w-1/3 xl:w-1/4 duration-300 hover:bg-blue-400 hover:scale-105"}>
+                " w-full sm:w-full md:w-full lg:w-1/3 xl:w-1/4 " +
+                "duration-300 hover:bg-blue-400 hover:scale-105"}>
             <ul>
                 <img src={`https://robohash.org/${id}`}
                      alt="Robo photo" width="200px" height="200px"
@@ -12,5 +13,7 @@ export const Card = ({id, name, email}) => {
                 <li className={"text-center sm:text2xl md:text-base  text-xl"}>{email}</li>
             </ul>
         </div>
+
+
     )
 }
