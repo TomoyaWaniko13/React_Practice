90d6c839c681f3784bb10d8ec0fcfd32c5cac3b1
TomoyaWaniko <alligatorfree12@gmail.com>
2023-11-28 17:22:04 -0800

    Introduce useState

--- todo-list-tailwind-jyakee3/src/Components/FinishedArea.jsx
+++ todo-list-tailwind-jyakee3/src/Components/FinishedArea.jsx
@@ -1,15 +1,25 @@
+import {useState} from "react";
+
 export const FinishedArea = () => {
+    const [finishedTasks, setFinishedTasks] = useState(["Task neko san 100", "Task neko san 101"])
+
     return (
         <>
             <h1 className={"text-4xl font-bold my-10 text-center"}>Finished Tasks</h1>
-            <ul className={"flex items-center"}>
-                <li><p className={"mx-10 text-2xl font-semibold"}>Task 1 Neko-san</p></li>
-                <li>
-                    <button
-                        className={"border-2 border-black text-2xl p-5 m-2  rounded-3xl hover:bg-black hover:text-white"}>Undo
-                    </button>
-                </li>
+
+            <ul>
+                {finishedTasks.map((task) => {
+                    return (
+                        <div className={"flex items-center"}>
+                            <li><p className={"mx-10 text-2xl font-semibold"}>{task}</p></li>
+                            <button
+                                className={"border-2 border-black text-2xl p-5 m-2  rounded-3xl hover:bg-black hover:text-white"}>Undo
+                            </button>
+                        </div>
+                    )
+                })}
             </ul>
+
         </>
     )
 }
--- todo-list-tailwind-jyakee3/src/Components/UnfinishedArea.jsx
+++ todo-list-tailwind-jyakee3/src/Components/UnfinishedArea.jsx
@@ -1,34 +1,32 @@
+import {useState} from "react";
+
+
 export const UnfinishedArea = () => {
+    const [unfinishedTasks, setUnfinishedTasks] = useState(["Task 1 neko san", "Task 2 Neko san"])
     return (
         <>
             <div>
                 <h1 className={"text-4xl font-bold my-10 text-center"}>Unfinished Tasks</h1>
-                <ul className={"flex items-center"}>
-                    <li><p className={"mx-10 text-2xl font-semibold"}>Task 1 Neko-san</p></li>
-                    <li>
-                        <button
-                            className={"border-2 border-black text-2xl p-5 m-2  rounded-3xl hover:bg-black hover:text-white"}>Done
-                        </button>
-                    </li>
-                    <li>
-                        <button
-                            className={"border-2 border-black text-2xl p-5 m-2  rounded-3xl hover:bg-black hover:text-white"}>Delete
-                        </button>
-                    </li>
+
+                <ul>
+                    {unfinishedTasks.map((task) => {
+                        return (
+                            <li>
+                                <div className={"flex items-center"}>
+                                    <p className={"mx-10 text-2xl font-semibold"}>{task}</p>
+                                    <button
+                                        className={"border-2 border-black text-2xl p-5 m-2  rounded-3xl hover:bg-black hover:text-white"}>Done
+                                    </button>
+                                    <button
+                                        className={"border-2 border-black text-2xl p-5 m-2  rounded-3xl hover:bg-black hover:text-white"}>Delete
+                                    </button>
+                                </div>
+                            </li>
+                        )
+                    })}
                 </ul>
-                <ul className={"flex items-center"}>
-                    <li><p className={"mx-10 text-2xl font-semibold"}>Task 1 Neko-san</p></li>
-                    <li>
-                        <button
-                            className={"border-2 border-black text-2xl p-5 m-2  rounded-3xl hover:bg-black hover:text-white"}>Done
-                        </button>
-                    </li>
-                    <li>
-                        <button
-                            className={"border-2 border-black text-2xl p-5 m-2  rounded-3xl hover:bg-black hover:text-white"}>Delete
-                        </button>
-                    </li>
-                </ul>
+
+
             </div>
         </>
     )
