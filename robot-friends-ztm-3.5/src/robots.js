4e66ee4a1dfeb05d03d1190fb44e796bb9ff8e0b
TomoyaWaniko <alligatorfree12@gmail.com>
2023-12-07 10:35:48 -0800

    add onSearchChange()

--- robot-friends-ztm-3.5/src/robots.js
+++ robot-friends-ztm-3.5/src/robots.js
@@ -18,5 +18,30 @@ export const robots = [
         id:4,
         name:"yabai neko san",
         email:"neko@gmail.com"
+    },
+    {
+        id:5,
+        name:"kerokero neko san",
+        email:"neko@gmail.com"
+    },
+    {
+        id:6,
+        name:"Google san",
+        email:"neko@gmail.com"
+    },
+    {
+        id:7,
+        name:"Matrix neko san",
+        email:"neko@gmail.com"
+    },
+    {
+        id:8,
+        name:"Pepsi neko san",
+        email:"neko@gmail.com"
+    },
+    {
+        id:9,
+        name:"fat neko san",
+        email:"neko@gmail.com"
     }
 ]
