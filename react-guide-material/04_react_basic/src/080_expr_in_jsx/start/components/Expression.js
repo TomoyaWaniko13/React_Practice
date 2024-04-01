68636da9c67c2453dc530cbcb36b1ee9f77c0744
TomoyaWaniko <alligatorfree12@gmail.com>
2024-04-01 14:16:02 +0900

    Test

---
+++ react-guide-material/04_react_basic/src/080_expr_in_jsx/start/components/Expression.js
@@ -0,0 +1,9 @@
+const Expression = () => {
+    const title = 'This is a title.'
+    return (
+        <div>
+            <h3>Hello! {title}</h3>
+        </div>
+    );
+};
+export default Expression;
