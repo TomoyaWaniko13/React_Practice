f5eb98da8ac161dba7ca4feb3a73b7a2512fa587
TomoyaWaniko <alligatorfree12@gmail.com>
2024-04-02 13:25:35 +0900

    Test

--- react-guide-material/04_react_basic/src/080_expr_in_jsx/start/components/Expression.js
+++ react-guide-material/04_react_basic/src/080_expr_in_jsx/start/components/Expression.js
@@ -1,9 +1,23 @@
+import './Expression.css';
+
 const Expression = () => {
-    const title = 'This is a title.'
+    const title = 'Expression';
+
+    const array = ['item 1', 'item 2', 'item 3'];
+    const joinedArray = array.join(', ');
+
+    const hello = (arg) => `${arg} Function`;
+
+
     return (
-        <div>
+        <div className={title.toLowerCase()}>
             <h3>Hello! {title}</h3>
+            <h3>{joinedArray}</h3>
+            <h3>{hello('hello')}</h3>
+            <h3>{/*This line is not displayed because this is a comment.*/}</h3>
+            {<h3>Hello JSX</h3>}
         </div>
     );
 };
+
 export default Expression;
