f33afae55739321d9bb102b65d8091d9f593cc82
TomoyaWaniko <alligatorfree12@gmail.com>
2024-04-05 06:06:15 +0900

    Test

--- react-guide-material/05_state_and_event/src/064_state_array/start/Example.js
+++ react-guide-material/05_state_and_event/src/064_state_array/start/Example.js
@@ -1,11 +1,32 @@
-const Example = () => {
-  const numArray = [1, 2, 3, 4, 5];
+import {useState} from "react";
 
-  return (
-    <>
-      
-    </>
-  );
+const Example = () => {
+    // This numArray is an initial state.
+    const initialNumberArray = [1, 2, 3, 4, 5];
+    // eslint-disable-next-line no-undef
+    const [numberArray, setNumberArray] = useState(initialNumberArray);
+    console.log(numberArray);
+
+    const shuffle = () => {
+        const newNumberArray = [...numberArray];
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/pop
+        // The pop() method of Array instances removes the last element from
+        // an array and returns that element.
+        const lastElement = newNumberArray.pop();
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/unshift
+        // The unshift() method of Array instances adds the specified elements to
+        // the beginning of an array and returns the new length of the array.
+        newNumberArray.unshift(lastElement);
+        console.log(newNumberArray);
+        setNumberArray(newNumberArray);
+    }
+
+    return (
+        <>
+            <h1>{numberArray}</h1>
+            <button onClick={shuffle}>Shuffle!</button>
+        </>
+    );
 };
 
 export default Example;
--- react-guide-material/05_state_and_event/src/070_state_and_component/start/Example.js
+++ react-guide-material/05_state_and_event/src/070_state_and_component/start/Example.js
@@ -1,20 +1,42 @@
-import { useState } from "react";
+import {useState} from 'react';
 
 const Example = () => {
-  const [count, setCount] = useState(0);
-  const countUp = () => {
-    setCount((prevstate) => prevstate + 1);
-  };
-  const countDown = () => {
-    setCount(count - 1);
-  };
-  return (
-    <>
-      <h3>カウント: {count}</h3>
-      <button onClick={countUp}>+</button>
-      <button onClick={countDown}>-</button>
-    </>
-  );
+
+    const [toggle, setToggle] = useState(true);
+    const changeToggleValue = () => {
+        setToggle(prevState => !prevState);
+    }
+
+    return (
+        <>
+            <button onClick={changeToggleValue}>Toggle!!</button>
+            {toggle ? <Count key={'A'} title={'A'}/> : <Count key={'B'} title={'B'}/>}
+
+            {/*{toggle ? <Count title={'true'}/> : <div><Count title={'false'}/></div>}*/}
+
+            {/*<Count title={'A'}/>*/}
+            {/*{toggle && <Count title={'B'}/>}*/}
+        </>
+    )
+}
+
+const Count = ({title}) => {
+    const [count, setCount] = useState();
+
+    const countUp = () => {
+        setCount(prevState => prevState + 1);
+    }
+    const countDown = () => {
+        setCount(prevState => prevState - 1);
+    }
+
+    return (
+        <>
+            <h3>{title}: {count}</h3>
+            <button onClick={countUp}>+</button>
+            <button onClick={countDown}>-</button>
+        </>
+    );
 };
 
 export default Example;
