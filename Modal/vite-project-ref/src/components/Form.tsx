19b3e5faa1f3d3ab077e43578b9c4e5ead95d768
TomoyaWaniko <alligatorfree12@gmail.com>
2024-05-05 05:09:57 +0900

    Update

--- Modal/vite-project-ref/src/components/Form.tsx
+++ Modal/vite-project-ref/src/components/Form.tsx
@@ -1,16 +1,20 @@
+import MyInput from './MyInput.tsx';
+import { useRef } from 'react';
+
 function Form() {
-    const ref = useRef(null);
+  const ref = useRef(null);
 
-    function handleClick() {
-        ref.current.focus();
-    }
+  function handleClick() {
+    ref.current.focus();
+  }
 
-    return (
-        <form>
-            <MyInput label="Enter your name:" ref={ref} />
-            <button type="button" onClick={handleClick}>
-                Edit
-            </button>
-        </form>
-    );
+  return (
+    <form>
+      <MyInput label={'Enter your name:'} ref={ref} />
+      <button type='button' onClick={handleClick}>
+        Edit
+      </button>
+    </form>
+  );
 }
+export default Form;
--- Modal/vite-project-ref/src/components/MyInput.tsx
+++ Modal/vite-project-ref/src/components/MyInput.tsx
@@ -1,12 +1,12 @@
 import { forwardRef } from 'react';
 
 const MyInput = forwardRef(function MyInput(props, ref) {
-    const { label, ...otherProps } = props;
-    return (
-        <label>
-            {label}
-            <input {...otherProps} />
-        </label>
-    );
+  const { label, ...otherProps } = props;
+  return (
+    <label className={'border border-gray-300'}>
+      {label}
+      <input {...otherProps} ref={ref} className={'w-full h-full'} />
+    </label>
+  );
 });
 export default MyInput;
